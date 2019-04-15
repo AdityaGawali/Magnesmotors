906256d712d18e00e31e12ac31d3f01f58a0d319
AdityaGawali <adityagawali13@gmail.com>
2019-04-15 21:02:22 +0530

    ignore build

--- rs232/build/bootloader/soc/esp32/soc_memory_layout.d
+++
@@ -1,3 +0,0 @@
-esp32/soc_memory_layout.o: \
- /home/aditya/esp/esp-idf/components/soc/esp32/soc_memory_layout.c
-/home/aditya/esp/esp-idf/components/soc/esp32/soc_memory_layout.c:
--- serverwithsocket/build/libsodium/libsodium/src/libsodium/sodium/version.d
+++
@@ -1,9 +0,0 @@
-libsodium/src/libsodium/sodium/version.o: \
- /home/aditya/esp/esp-idf/components/libsodium/libsodium/src/libsodium/sodium/version.c \
- /home/aditya/esp/esp-idf/components/libsodium/port_include/sodium/version.h \
- /home/aditya/esp/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/export.h
-
-/home/aditya/esp/esp-idf/components/libsodium/port_include/sodium/version.h:
-
-/home/aditya/esp/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/export.h:
-/home/aditya/esp/esp-idf/components/libsodium/libsodium/src/libsodium/sodium/version.c:
