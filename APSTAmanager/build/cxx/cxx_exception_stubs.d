a18131de96387b2bd8f481103213e1a4d388163a
AdityaGawali <adityagawali13@gmail.com>
2019-07-23 01:09:22 +0530

    delete build

--- APSTAmanager/build/cxx/cxx_exception_stubs.d
+++
@@ -1,6 +0,0 @@
-cxx_exception_stubs.o: \
- /home/aditya/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
- /home/aditya/esp/espcodes/magnes/APSTAmanager/build/include/sdkconfig.h
-
-/home/aditya/esp/espcodes/magnes/APSTAmanager/build/include/sdkconfig.h:
-/home/aditya/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
